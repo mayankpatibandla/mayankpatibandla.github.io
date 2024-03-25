cbea3a3d3612193a2dd8411b3c0706102eeec789
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-03-25 19:13:16 -0400

    Testing animejs

--- scripts/dist/index.js
+++ scripts/dist/index.js
@@ -1,3 +1,10 @@
-"use strict";
-console.log("Hello, world!");
+import anime from "animejs/lib/anime.es.js";
+anime({
+    targets: "div",
+    translateX: 250,
+    rotate: "1turn",
+    backgroundColor: "#FFF",
+    duration: 800,
+    loop: true,
+});
 //# sourceMappingURL=index.js.map
--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import anime from "animejs/lib/anime.es.js";
+
+console.log("Starting animation...");
+
+anime({
+  targets: "div",
+  translateX: 250,
+  rotate: "1turn",
+  backgroundColor: "#FFF",
+  duration: 800,
+  loop: true,
+});
+
+console.log("Animation started.");
