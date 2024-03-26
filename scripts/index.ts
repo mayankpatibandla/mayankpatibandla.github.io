83835cb4571a52cef0a44027b6293dd8ff124dab
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-03-25 20:13:02 -0400

    Add grid effect

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,14 +1,67 @@
 import anime from "animejs/lib/anime.es.js";
 
-console.log("Starting animation...");
+// https://codepen.io/Hyperplexed/pen/zYWvXMM
+const wrapper = document.getElementById("tiles");
 
-anime({
-  targets: "div",
-  translateX: 250,
-  rotate: "1turn",
-  backgroundColor: "#FFF",
-  duration: 800,
-  loop: true,
-});
+let columns = 0,
+  rows = 0,
+  toggled = false;
 
-console.log("Animation started.");
+const toggle = () => {
+  toggled = !toggled;
+
+  document.body.classList.toggle("toggled");
+};
+
+const handleOnClick = (index: number) => {
+  toggle();
+
+  anime({
+    targets: ".tile",
+    opacity: toggled ? 0 : 1,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index,
+    }),
+  });
+};
+
+const createTile = (index: number) => {
+  const tile = document.createElement("div");
+
+  tile.classList.add("tile");
+
+  tile.style.opacity = toggled ? "0" : "1";
+
+  tile.onclick = () => handleOnClick(index);
+
+  return tile;
+};
+
+const createTiles = (quantity: number) => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    if (wrapper) {
+      wrapper.appendChild(createTile(index));
+    }
+  });
+};
+
+const createGrid = () => {
+  if (!wrapper) return;
+
+  wrapper.innerHTML = "";
+
+  const size = document.body.clientWidth > 800 ? 100 : 50;
+
+  columns = Math.floor(document.body.clientWidth / size);
+  rows = Math.floor(document.body.clientHeight / size);
+
+  wrapper.style.setProperty("--columns", columns.toString());
+  wrapper.style.setProperty("--rows", rows.toString());
+
+  createTiles(columns * rows);
+};
+
+createGrid();
+
+window.onresize = () => createGrid();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import anime from "animejs/lib/anime.es.js";
-console.log("Starting animation...");
-anime({
-    targets: "div",
-    translateX: 250,
-    rotate: "1turn",
-    backgroundColor: "#FFF",
-    duration: 800,
-    loop: true,
-});
-console.log("Animation started.");
+// https://codepen.io/Hyperplexed/pen/zYWvXMM
+const wrapper = document.getElementById("tiles");
+let columns = 0, rows = 0, toggled = false;
+const toggle = () => {
+    toggled = !toggled;
+    document.body.classList.toggle("toggled");
+};
+const handleOnClick = (index) => {
+    toggle();
+    anime({
+        targets: ".tile",
+        opacity: toggled ? 0 : 1,
+        delay: anime.stagger(50, {
+            grid: [columns, rows],
+            from: index,
+        }),
+    });
+};
+const createTile = (index) => {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    tile.style.opacity = toggled ? "0" : "1";
+    tile.onclick = () => handleOnClick(index);
+    return tile;
+};
+const createTiles = (quantity) => {
+    Array.from(Array(quantity)).map((tile, index) => {
+        if (wrapper) {
+            wrapper.appendChild(createTile(index));
+        }
+    });
+};
+const createGrid = () => {
+    if (!wrapper)
+        return;
+    wrapper.innerHTML = "";
+    const size = document.body.clientWidth > 800 ? 100 : 50;
+    columns = Math.floor(document.body.clientWidth / size);
+    rows = Math.floor(document.body.clientHeight / size);
+    wrapper.style.setProperty("--columns", columns.toString());
+    wrapper.style.setProperty("--rows", rows.toString());
+    createTiles(columns * rows);
+};
+createGrid();
+window.onresize = () => createGrid();
 //# sourceMappingURL=index.js.map
