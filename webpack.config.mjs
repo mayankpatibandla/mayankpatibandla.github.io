5b583f1cc7b8ae92d349978aa7275b11ac0c0bbd
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-05-04 19:03:58 -0400

    Fix directory names and pack ts directly

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -2,12 +2,25 @@ import TerserPlugin from "terser-webpack-plugin";
 
 export default {
   entry: {
-    index: "./src/index.js",
+    index: "./src/index.ts",
   },
   output: {
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   mode: "production",
+  devtool: "inline-source-map",
   optimization: {
     minimizer: [
       new TerserPlugin({
